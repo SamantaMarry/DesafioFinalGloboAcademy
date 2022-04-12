c50efff0e72c10b8896c9de80b47d0033c60cb6e
Samanta Marry <samantamaarry@gmail.com>
2022-04-11 21:23:50 -0300

    Merge

---
+++ api/src/modal/products.py
@@ -0,0 +1,19 @@
+from api.src.modal.modal_base import ModelBase
+
+
+class ProductsModel(ModelBase):
+    __tablename__ = "products"
+    __columns__ = ("id", "name", "url_image", "description", "price", "extras", "id_restaurant")
+
+    id = None
+
+
+    def build(self, name, url_image, description, price, extras, id_restaurant, id=None):
+        self.id = id
+        self.name = name
+        self.url_image = url_image
+        self.description = description
+        self.price = price
+        self.extras = extras
+        self.id_restaurant = id_restaurant
+        return self
