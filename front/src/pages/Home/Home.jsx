4becac6b480afcf1ec7795249d0e45e009e77bf4
allan.fsgarcia <allan.fsgarcia@outlook.com>
2022-04-12 21:21:19 -0300

    adding footer

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -52,8 +52,39 @@ export default function Home() {
               </section>
           </article>
         </main>
-        <footer></footer>
-      </body>
+        <footer>
+          <section>
+            <p>onde estamos</p>
+            <ul>
+                <li><a href="#">recife</a></li>
+                <li><a href="#">joão Pessoa</a></li>
+                <li><a href="#">são paulo</a></li>
+                <li><a href="#">rio de janeiro</a></li>
+                <li><a href="#">belo horizonte</a></li>
+                <li><a href="#">porto alegre</a></li>
+              </ul>
+          </section>
+          <section>
+            <p>contato</p>
+            <ul>
+                <li><a href="#">facebook.com/JasonsFood</a></li>
+                <li><a href="#">twitter.com/JasonsFood</a></li>
+                <li><a href="#">instagram.com/JasonsFood</a></li>
+                <li><a href="#">contatojasonsfood@gmail.com</a></li>
+                <li><a href="#">tel:  11 9 9999-9999</a></li>
+                
+              </ul>
+          </section>
+          <section>
+            <p>Jason's food</p>
+            <ul>
+                <li><a href="#">Políticas e termos</a></li>
+                <li><a href="#">Sobre o Jason's Food</a></li>
+                            </ul>
+          </section>
 
+        </footer>
+      </body>
+</>
   )
 }
