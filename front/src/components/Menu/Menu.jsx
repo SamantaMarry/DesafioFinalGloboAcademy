f737756a1b591b618f4fce9949d69f46442c67d4
igormotta92 <igor.mottta@gmail.com>
2022-04-14 12:32:35 -0300

    merge :  merge menu

--- front/src/components/Menu/Menu.jsx
+++ front/src/components/Menu/Menu.jsx
@@ -8,30 +8,24 @@ import { Link } from 'react-router-dom';
 function Menu() {
   return (
     <>
-<<<<<<< HEAD
-      <div id="logo-links">
+      <Link to={'/'}>
         <div id="logo">
           <img src={logo} />
-=======
-    <Link to={'/'}>
-    <div id="logo">
-        <img src={logo} />
-      </div>
-    </Link>
+        </div>
+      </Link>
       <nav class="menu">
         <div class="links-menu">
           <ul>
             <Link to={'/'}>
-            <li><a href="">Restaurantes</a></li>
+              <li><a href="">Restaurantes</a></li>
             </Link>
-            <Link to = {'/admin/restaurant/cadastro'}>
-            <li><a href=""> Cadastre seu Restaurante</a></li>
+            <Link to={'/admin/restaurant/cadastro'}>
+              <li><a href=""> Cadastre seu Restaurante</a></li>
             </Link>
             <Link to={'/admin/product/cadastro'}>
-            <li><a href="">Cadastre seu Produto</a></li>
+              <li><a href="">Cadastre seu Produto</a></li>
             </Link>
           </ul>
->>>>>>> main
         </div>
         <nav class="menu">
           <div class="links-menu">
@@ -40,8 +34,8 @@ function Menu() {
               <li><a href="">Trabalhe conosco</a></li>
               <li><a href="">Entregas</a></li>
             </ul>
-          </div>        
-      </nav>
+          </div>
+        </nav>
       </div>
       <div class="login-area">
         <a href="">Entrar</a>
